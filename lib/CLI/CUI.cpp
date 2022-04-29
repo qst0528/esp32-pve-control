14c858aebaab36dd138390ff251f9cad198aec0d
Suto, Masanori <suto@geek-masa.com>
2022-04-29 12:12:35 +0900

    Add CUI module

---
+++ lib/CLI/CUI.cpp
@@ -0,0 +1,38 @@
+#include "CUI.hpp"
+#include "Log.hpp"
+
+namespace {
+int printVersion(int argc = 0, char **argv = nullptr) {
+  shell.println(F(">> PVE Control"));
+  shell.println(F(">>   Git revision: " GIT_REV));
+  shell.println(F(">>   Build date:   " __DATE__));
+  return EXIT_SUCCESS;
+}
+} // namespace
+
+struct CUI::LookupVals {
+    char *name;
+    int val;
+};
+
+int CUI::lookup(const char *aName, const CUI::lookupVals entries[]) {
+  int i = 0;
+  for (; entries[i].name; i++) {
+    if (strncasecmp(aName, entries[i].name, 20) == 0) {
+      break;
+    }
+  }
+  return entries[i].val;
+}
+
+void CUI::attach() {
+  Log.trace(F("Attaching SimpleShell to Serial..." CR));
+  shell.attach(REQUESTER);
+  Log.notice(F("SimpleShell attached." CR));
+  Log.notice(F("Type \"help\" to get list of commands." CR));
+}
+
+void CUI::registerCUI() {
+  Log.info(F("Adding basic CLI commands..." CR));
+  shell.addCommand(F("version"), printVersion);
+}
---
+++ lib/CLI/CUI.hpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+#include <SimpleSerialShell.h>
+
+namespace CUI {
+  extern Stream &REQUESTER;
+  typedef struct LookupVals lookupVals;
+  int  lookup(const char *, const lookupVals[]);
+  void attach();
+  void registerCUI();
+} // namespace CUI
