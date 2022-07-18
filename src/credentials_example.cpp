5620df0011d705f0096d6aa8e915830494d23450
Suto, Masanori <suto@geek-masa.com>
2022-07-19 01:13:16 +0900

    Add example credential

---
+++ src/credentials_example.cpp
@@ -0,0 +1,6 @@
+#include "config.hpp"
+
+char const* IP::SSID       = "MyAccessPoint";
+char const* IP::PASSPHRASE = "verySecurePassphrase";
+
+char const* API::KEY       = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
