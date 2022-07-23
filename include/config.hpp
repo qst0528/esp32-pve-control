a3ef46ae0d797fd9f0faefd35f18863443363d28
Suto, Masanori <suto@geek-masa.com>
2022-07-24 00:01:56 +0900

    Make cosmetic changes

--- include/config.hpp
+++ include/config.hpp
@@ -1,19 +1,17 @@
 #include <Arduino.h>
 #include <CUI.hpp>
-#include <Log.hpp>
 #include <IP.hpp>
+#include <Log.hpp>
 
 namespace API {
-  extern char const* KEY;
+extern char const *TOKEN;
 }
 
 namespace SSL {
-  extern const uint8_t root_ca_cert_start[]
-  asm("_binary_src_certs_root_ca_pem_start");
-  extern const uint8_t root_ca_cert_end[]
-  asm("_binary_src_certs_root_ca_pem_end");
-  extern const uint8_t ssl_pub_start[]
-  asm("_binary_src_certs_ssl_pub_pem_start");
-  extern const uint8_t ssl_pub_end[]
-  asm("_binary_src_certs_ssl_pub_pem_end");
+    extern const uint8_t
+    root_ca_cert_start[] asm("_binary_src_certs_root_ca_pem_start");
+    extern const uint8_t
+    root_ca_cert_end[] asm("_binary_src_certs_root_ca_pem_end");
+    extern const uint8_t ssl_pub_start[] asm("_binary_src_certs_ssl_pub_pem_start");
+    extern const uint8_t ssl_pub_end[] asm("_binary_src_certs_ssl_pub_pem_end");
 }; // namespace SSL
