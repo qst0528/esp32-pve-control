cbb5f237d95a3b57e4d58dee1478b8b7e924eaf6
Suto, Masanori <suto@geek-masa.com>
2022-07-24 00:02:06 +0900

    Rewrite main routine; achieved first operating code

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,32 @@
 #include "config.hpp"
-
-#include <HTTPClient.h>
-#include <WiFiClientSecure.h>
+#include "HTTPClient.h"
 
 namespace {
-void serialSetup(HardwareSerial &serial, unsigned long baud) {
-  serial.begin(baud);
-  serial.println(F("Booting PVE Control..."));
-}
+    void serialSetup(HardwareSerial &serial, unsigned long baud) {
+        serial.begin(baud);
+        serial.println(F("Booting PVE Control..."));
+    }
+    HTTPClient https;
 } // namespace
 
 void setup() {
-  serialSetup(Serial, 115200);
-  LOG::setup();
-  CUI::attach();
-  CUI::registerCUI();
-  IP::init();
-  delay(5000);
+    serialSetup(Serial, 115200);
+    LOG::setup();
+    CUI::attach();
+    CUI::registerCUI();
+    IP::init();
+    https.setReuse(true);
+    delay(5000);
 }
 
 // put your setup code here, to run once:
 void loop() {
-  shell.executeIfInput();
-  // put your main code here, to run repeatedly:
+    shell.executeIfInput();
+    // put your main code here, to run repeatedly:
 
-  WiFiClientSecure *client = new WiFiClientSecure;
-  client -> setCACert( (char *) SSL::root_ca_cert_start );
-
-  if(client) {
-
-      {
-          HTTPClient https;
-
-          if (https.begin(*client, "https://pve.home.intranet:8006/api2/json/"
-                          "nodes/pve/qemu/400/status/start")) {
-
-              https.setAuthorization("PVEAPIToken=guest@pve!vmpower=%s", API::KEY);
-              Log.infoln(F("Return code: %d"), https.POST("node=pve&vmid=400"));
-              https.end();
-          } else {
-              Log.infoln("[HTTPS] Unable to connect");
-          }
-      }
-      delete client;
-  } else {
-      Log.infoln("Unable to create client");
-  }
-  delay(60000);
+    https.setAuthorization(API::TOKEN);
+    https.begin("pve.home.intranet", 8006, "/api2/json/nodes/pve/qemu/400/status/start", (char *) SSL::root_ca_cert_start);
+    https.POST("node=pve&vmid=400");
+    https.end();
+    delay(60000);
 }
